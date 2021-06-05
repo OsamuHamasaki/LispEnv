b3d95e7a43c39f644fc167da2de1bc0ced24f9a4
Osamu Hamasaki <osamu.hamasaki@gmail.com>
2021-06-05 21:25:00 +0900

    Add some macro utilities.

--- utility.lisp
+++ utility.lisp
@@ -55,6 +55,43 @@
                max score))))
       (values wins max))))
 
+;;;; Macro Utilities from Paul Graham's ANSI Common Lisp
+
+;;; for
+
+(defmacro for (var start stop &body body)
+ (let ((gstop (gensym)))
+  `(do ((,var ,start (1+ ,var))
+        (,gstop ,stop))
+       ((> ,var ,gstop))
+    ,@body)))
+
+(defmacro in (obj &rest choices)
+ (let ((insym (gensym)))
+  `(let ((,insym ,obj))
+    (or ,@(mapcar #'(lambda (c) `(eql ,insym ,c))
+           choices)))))
+
+(defmacro random-choice (&rest exprs)
+ `(case (random ,(length exprs))
+     `@(lst ((key -1))
+         (mapcar #'(lambda (expr)
+                    `(,(incf key) ,expr))
+                 exprs))))
+
+(defmacro avg (&rest args)
+ `(/ (+ ,@args) ,(length args)))
+
+(defmacro with-gensym (syms &body body)
+ `(let ,(mapcar #'(lambda (s)
+                    `(,s (gensym)))
+                syms)
+     ,@body))
+
+(defmacro aif (test then &optional else)
+ `(let ((it ,test))
+     (if it ,then ,else)))
+
 
 ;;;; My Original 
 
